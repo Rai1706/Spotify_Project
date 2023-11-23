a62b3e88a0797ea7a99d4adfea54760bc12812a4
Shashank Kumar Dubey <shashankkdubey1010@gmail.com>
2023-11-23 23:35:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,16 @@
-<?php  
+<?php
 include("dbconnect.php");
 ?>
 <?php
-$un=$_POST['t1'];
-$pd=$_POST['t2'];
-$sql = "SELECT * FROM student";
+$un = $_POST['t1'];
+$pd = $_POST['t2'];
+$sql = "SELECT * FROM lgn";
 $result = mysqli_query($conn, $sql);
 
-while($row = mysqli_fetch_assoc($result)) 
-{
-    if($row["uname"]==$un && $row["pass"]== $pd)
-     {
-      header("location:profile.php");
-     }
-} 
+while ($row = mysqli_fetch_assoc($result)) {
+    if ($row["uname"] == $un && $row["pass"] == $pd) {
+        header("location:profile.php");
+    }
+}
 mysqli_close($conn);
 ?>
