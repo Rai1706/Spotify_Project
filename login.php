028d157b869fd2457046417fcff30d1d9b062f30
Rai1706 <rachit.rai.1706@gmail.com>
2023-11-23 18:10:34 +0530

    Rachit: adding all files into Spotify folder

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php  
+include("dbconnect.php");
+?>
+<?php
+$un=$_POST['t1'];
+$pd=$_POST['t2'];
+$sql = "SELECT * FROM student";
+$result = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($result)) 
+{
+    if($row["uname"]==$un && $row["pass"]== $pd)
+     {
+      header("location:profile.php");
+     }
+} 
+mysqli_close($conn);
+?>
---
+++ reg.php
@@ -0,0 +1,29 @@
+<?php 
+$un=$_POST['t1'];
+$pd=$_POST['t2'];
+$em=$_POST['t3'];
+$mb=$_POST['t4'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "abes";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn)
+ {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  
+$sql = "INSERT INTO student (uname, pass, emailid,mobno)
+  VALUES ('$un','$pd','$em','$mb')";
+  
+  if (mysqli_query($conn, $sql))
+   {
+    echo "New record created successfully";
+    }
+    else
+     {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+?>
